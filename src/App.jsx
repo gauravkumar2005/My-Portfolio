0544661c9990d3a54c17d21b71917e6aa76738af
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-03-03 17:23:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
+import Skills from "./pages/Skills";
+
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
