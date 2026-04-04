31aac84ad873eee31b1d339926a7e7232341de7f
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-04 18:08:27 +0530

    change kiya

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>this is my second react app</p>
+      
+    </>
+  )
+}
+
+export default App
