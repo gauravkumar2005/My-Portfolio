e6de36ee06fbadd5d56f9763587ef409837b5306
gauravkumar2005 <gauravkumar8414pq@gmail.com>
2026-04-29 16:01:23 +0530

    update code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
             href="#projects"
             className="inline-block px-5 sm:px-6 py-2 sm:py-3 bg-blue-500 rounded-lg hover:bg-blue-600 transition"
           >
-        
+            View My Work
           </a>
         </div>
 
