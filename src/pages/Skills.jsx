41f96864fef97d0d7794c512ade6e69fee0c7e27
Gaurav Kumar <gauravkumar8414pq@gmail.com>
2026-06-25 20:57:51 +0530

    add learning card in skill section

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -22,6 +22,7 @@ function Skills() {
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">JavaScript</li>
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">React</li>
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">Tailwind CSS</li>
+              <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">Next.js</li>
             </ul>
           </div>
 
@@ -33,6 +34,7 @@ function Skills() {
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">Node.js</li>
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">Express.js</li>
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">MongoDB</li>
+              <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">REST API</li>
             </ul>
           </div>
 
@@ -45,8 +47,30 @@ function Skills() {
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">GitHub</li>
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">VS Code</li>
               <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">Postman</li>
+              <li className="bg-gray-800 px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300 cursor-pointer">Vercel</li>
             </ul>
           </div>
+
+          <div className="bg-gray-900 p-6 rounded-2xl border border-gray-800 shadow-lg hover:scale-105 hover:border-blue-500 hover:shadow-blue-500/20 hover:shadow-xl transition-all duration-300">
+
+           <h3 className="text-2xl font-bold mb-6 text-blue-300">
+             Currently Learning
+           </h3>
+
+           <ul className="space-y-3">
+           <li className="bg-gray-800 px-4 py-2 rounded-lg">
+             Data Structures & Algorithms
+           </li>
+
+           <li className="bg-gray-800 px-4 py-2 rounded-lg">
+             Next.js
+           </li>
+
+          <li className="bg-gray-800 px-4 py-2 rounded-lg">
+            Backend Development
+          </li>
+        </ul>
+        </div>
 
         </div>
 
